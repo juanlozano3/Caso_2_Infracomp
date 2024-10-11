e91759f7d70eadd8c8228070fdabc674bfea438b
Juanes Alvarez <je.alvarezg1@uniandes.edu.co>
2024-10-11 11:37:05 -0500

    Main con opciones terminadas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
@@ -13,14 +14,21 @@ public class App {
         System.out.print("Digite la opción que desea: ");
         System.out.print("1) Generación de las referencias. ");
         System.out.print("2) Calcular datos buscados. ");
+        System.out.print("3) Esconder mensaje. ");
+        System.out.print("4) Recuperar mensaje. ");
 
         // Leer un número
         System.out.print("Ingrese un número: ");
         int opcion = scanner.nextInt();
         scanner.close();
         // Cerrar el scanner
-
         if (opcion == 1) {
+            //Generar referencias
+        }
+        else if (opcion == 2) {
+            //Calcular datos
+        }
+        else if (opcion == 3) {
             InputStreamReader isr = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(isr);
             try {
@@ -42,18 +50,22 @@ public class App {
                 e.printStackTrace();
             }
 
-        } else if (opcion == 2) {
+        } else if (opcion == 4) {
             InputStreamReader isr = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(isr);
             try {
-                System.out.println("Nombre del archivo con el mensaje escondido: ");
-                ruta = br.readLine();
+                System.out.println("Nombre de la imagen con el mensaje escondido: ");
+                String ruta = br.readLine();
                 System.out.println("Nombre del archivo para almacenar el mensaje recuperado: ");
                 String salida = br.readLine();
-                imagen = new Imagen(ruta);
+                Imagen imagen = new Imagen(ruta);
                 int longitud = imagen.leerLongitud();
-                mensaje = new char[longitud];
+                char[] mensaje = new char[longitud];
                 imagen.recuperar(mensaje, longitud);
+                FileWriter writer = new FileWriter(salida);
+                writer.write(mensaje);
+                writer.close();
+                System.out.println("El mensaje ha sido recuperado y guardado en: " + salida);
             } catch (Exception e) {
                 e.printStackTrace();
             }
