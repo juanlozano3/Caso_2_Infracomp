65ab7f2e84936904c247953649e4a9408570098c
Juanes Alvarez <je.alvarezg1@uniandes.edu.co>
2024-10-15 14:23:39 -0500

    Cambios de tabla paginas a ram

--- src/RAM.java
+++ src/RAM.java
@@ -1,11 +1,14 @@
+import java.util.HashMap;
+
 public class RAM {
-     
+
      private Pagina[] marcos;
-     
+     private HashMap<int[], int[]> tabla_paginas = new HashMap<>();
+
      public RAM(int cantidadMarcos) {
           this.marcos = new Pagina[cantidadMarcos];
-          for (int i = 0 ; i < cantidadMarcos ; i++) {
-               int[] clave = {-1, 0}; 
+          for (int i = 0; i < cantidadMarcos; i++) {
+               int[] clave = { -1, 0 };
                Pagina pagina = new Pagina(clave);
                this.marcos[i] = pagina;
           }
@@ -18,5 +21,28 @@ public class RAM {
      public void setMarcos(Pagina[] marcos) {
           this.marcos = marcos;
      }
-     
+
+     // Estor etorna el ID de la pagina que se va a sacar de RAM para meter la nueva
+     public int LRU() {
+          int idLRU = -1;
+          int puntajeLRU = Integer.MAX_VALUE;
+          for (Pagina pagina : marcos) {
+               int puntaje = pagina.getPuntaje(tabla_paginas);
+               if (puntaje < puntajeLRU) {
+                    puntajeLRU = puntaje;
+                    idLRU = pagina.getId();
+               }
+          }
+          return idLRU;
+     }
+
+     public void actualizarBit(String bit, int[] clave) {
+          if (bit == "R") this.tabla_paginas.get(clave)[0] = 1;
+          else this.tabla_paginas.get(clave)[1] = 1;
+     }
+
+     public void cargarClave(int[] clave) {
+          this.tabla_paginas.put(clave, new int[] { 0, 0 });
+     }
+
 }
--- src/Simulador.java
+++ src/Simulador.java
@@ -1,42 +1,26 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
 
 public class Simulador {
 
-    private HashMap<int[], int[]> tabla_paginas = new HashMap<>();
     private RAM ram;
     private ArrayList<String[]> referencias = new ArrayList<>();
     private int hits, misses;
 
     public Simulador(String rutaReferencias, int marcosPagina) {
-        tabla_paginas = new HashMap<>();
-        int P = 0;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(rutaReferencias));
             String line;
 
             while ((line = reader.readLine()) != null) {
-                if (line.startsWith("P")) {
-                    String[] partes = line.split("=");
-                    P = Integer.parseInt(partes[1].trim());
-                }
                 if (line.startsWith("Imagen") || line.startsWith("Mensaje")) {
                     // Procesar la línea
                     String[] partes = line.split(",");
                     referencias.add(partes);
                     // Obtener la página y el desplazamiento de la imagen
-                    int pagina = Integer.parseInt(partes[1].trim());
-                    int desplazamiento = Integer.parseInt(partes[2].trim());
-                    int[] clave = new int[] { pagina, desplazamiento }; // Clave del HashMap
-
-                    int[] valor = new int[] { 0, 0 }; // Clave del HashMap
-
-                    // Insertar en el HashMap
-                    tabla_paginas.put(clave, valor);
                 }
             }
 
@@ -44,10 +28,6 @@ public class Simulador {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // imprimirPrimeras17Claves();
-
-        // estructura de marcos de página
-        // lista de listas de arreglos de tamaño 2
         this.ram = new RAM(marcosPagina);
     }
 
@@ -74,12 +54,13 @@ public class Simulador {
                     // actualizar el bit (R) en la tabla pagina
                     // R--M
                     // esto es sincronizado
-                    tabla_paginas.get(clave)[0] = 1;
+                    cargarEnRAM(clave);
+
+                    ram.actualizarBit("R", clave);
                     if (referencia[0].trim() == "Mensaje")
-                        tabla_paginas.get(clave)[1] = 1;
+                        ram.actualizarBit("M", clave);
 
                     // Esto no es sincornizado
-                    cargarEnRAM(clave);
 
                     try {
                         // Simulación del tiempo de procesamiento
@@ -106,20 +87,8 @@ public class Simulador {
         // Join para retornar total de hit y misses
     }
 
-    public void imprimirPrimeras17Claves() {
-        int contador = 0;
-        for (Map.Entry<int[], int[]> entry : tabla_paginas.entrySet()) {
-            System.out.println("Clave: Página " + entry.getKey()[0] + ", Desplazamiento " + entry.getKey()[1]);
-            System.out.println("Valor: [" + entry.getValue()[0] + ", " + entry.getValue()[1] + "]");
-            contador++;
-            if (contador >= 17) {
-                break;
-            }
-        }
-        System.out.println(tabla_paginas.size());
-    }
-
     public void cargarEnRAM(int[] clave) {
+        ram.cargarClave(clave);
         boolean hit = false;
         for (Pagina pagina : ram.getMarcos()) {
             if (pagina.getId() == clave[0]) {
@@ -130,7 +99,7 @@ public class Simulador {
         }
         if (!hit) {
             misses++;
-            int idPaginaEliminada = LRU();
+            int idPaginaEliminada = ram.LRU();
             for (Pagina pagina : ram.getMarcos()) {
                 if (pagina.getId() == idPaginaEliminada) {
                     Pagina nuevaPagina = new Pagina(clave);
@@ -140,17 +109,4 @@ public class Simulador {
         }
     }
 
-    // Estor etorna el ID de la pagina que se va a sacar de RAM para meter la nueva
-    public int LRU() {
-        int idLRU = -1;
-        int puntajeLRU = Integer.MAX_VALUE;
-        for (Pagina pagina : ram.getMarcos()) {
-            int puntaje = pagina.getPuntaje(tabla_paginas);
-            if (puntaje < puntajeLRU) {
-                puntajeLRU = puntaje;
-                idLRU = pagina.getId();
-            }
-        }
-        return idLRU;
-    }
 }
